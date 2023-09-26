2d5048c7f3aa9004879ca44afc5d4e7e774f83c9
Yuri Gurgenidze <yuri.gurgenidze@adyen.com>
2023-09-26 14:17:37 +0200

    add docs

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdyenPOS",
-	    url: "https://api.github.com/repos/Adyen/adyen-pos-mobile-ios-artifacts/releases/assets/127634463.zip",
-  	    checksum: "5e7b45be0c55bd03213304eb16b88ef9e273293992c96034a382a2cbfe24a630"
+            url: "https://api.github.com/repos/Adyen/adyen-pos-mobile-ios-artifacts/releases/assets/127634463.zip",
+            checksum: "5e7b45be0c55bd03213304eb16b88ef9e273293992c96034a382a2cbfe24a630"
         ),
         .binaryTarget(
             name: "ADYPOS",
             url: "https://api.github.com/repos/Adyen/adyen-pos-mobile-ios-artifacts/releases/assets/127634461.zip",
-   	    checksum: "bc35844235a8bdd8ef817fa603453355d537e7f6dd190724f7d671a46167e01b"
+            checksum: "bc35844235a8bdd8ef817fa603453355d537e7f6dd190724f7d671a46167e01b"
         )
     ]
 )
