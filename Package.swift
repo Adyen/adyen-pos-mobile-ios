7892aa3bc8cfabb3142d3592914dffe9c4ed8dc5
thomast <thomas.tielgroenestege@adyen.com>
2024-04-10 09:56:19 +0200

    Release 2.9.2

--- Package.swift
+++ Package.swift
@@ -6,14 +6,20 @@ import PackageDescription
 let package = Package(
     name: "AdyenPOS",
     products: [
-        .library(name: "AdyenPOS", targets: ["AdyenPOS"])
+        .library(name: "AdyenPOS", targets: ["AdyenPOS"]),
+        .library(name: "ADYPOS", targets: ["ADYPOS", "AdyenPOS"])
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
             name: "AdyenPOS",
-    	    url: "https://api.github.com/repos/Adyen/adyen-pos-mobile-ios-beta/releases/assets/158870004.zip",
-      	    checksum: "938087f527d0ae5a6260082aa72f247b0c58b7c76c53187d210b0ba25d43b284"
+    	    url: "https://api.github.com/repos/Adyen/adyen-pos-mobile-ios-beta/releases/assets/161282867.zip",
+      	    checksum: "0bdab84d622e166ffe9ca5ee275e6117cb7be75ab05ec6254d474d13df34ea5a"
+        ),
+        .binaryTarget(
+            name: "ADYPOS",
+            url: "https://api.github.com/repos/Adyen/adyen-pos-mobile-ios-beta/releases/assets/161282865.zip",
+           checksum: "f5053aa379a76c140a8e419088191f3312b382f32ed8d9fa186fba3afb9b7f64"
         )
     ]
 )
