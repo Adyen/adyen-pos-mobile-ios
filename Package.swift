612764477682b643bb3c8efc15c6b5f9c7ce6a9d
thomast <thomas.tielgroenestege@adyen.com>
2024-08-06 16:04:17 -0500

    3.1.1 Test Build

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdyenPOS",
-            url: "https://api.github.com/repos/Adyen/adyen-pos-mobile-ios-beta/releases/assets/183249186.zip",
-            checksum: "395af178b88a77473e0e7ad9cae6944414a90890b0a410e3cc7bceedeaaf4187"
+            url: "https://api.github.com/repos/Adyen/adyen-pos-mobile-ios-beta/releases/assets/184297490.zip",
+            checksum: "ac7d24088d6e602005eb1ba78a6db020585563a4de523ee07d0f018fade131cf"
         ),
         .binaryTarget(
             name: "ADYPOS",
-            url: "https://api.github.com/repos/Adyen/adyen-pos-mobile-ios-beta/releases/assets/183249202.zip",
-            checksum: "47e4585fab7ba678220705472c07402f8da4e6d16d2467def273450fdefc01c9"
+            url: "https://api.github.com/repos/Adyen/adyen-pos-mobile-ios-beta/releases/assets/184297497.zip",
+            checksum: "0c416ed510a85225df43849adf6de60234d8776f5819d6152941d0b4fe7df625"
         )
     ]
 )
