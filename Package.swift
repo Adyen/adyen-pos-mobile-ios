d6bb33e31fe80a793911565513ac3cd8598d1ee5
James <17139737+mcguire-james@users.noreply.github.com>
2025-09-05 13:18:54 +0200

    Release 3.10.0

--- Package.swift
+++ Package.swift
@@ -15,23 +15,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdyenPOSTEST",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.9.2/Test/AdyenPOS.xcframework.zip",
-            checksum: "254536220d193d48826cf3f2b511dbe9f88e221568c2cbb951855b3f04695d59"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.10.0/Test/AdyenPOS.xcframework.zip",
+            checksum: "d06ac6e29d59d27a435010313b609ef26bd16d429bfbedb9d12bf4afa20989fc"
         ),
         .binaryTarget(
             name: "ADYPOSTEST",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.9.2/Test/ADYPOS.xcframework.zip",
-            checksum: "1dc7a8ab993e2d6a76d67c07b0899719bc822f0929945bf4ff46b2a281e01b64"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.10.0/Test/ADYPOS.xcframework.zip",
+            checksum: "b5554750ec88f193701d4c143b2f339a9668eb14956c401ab48e7c100efc12d8"
         ),
         .binaryTarget(
             name: "AdyenPOSLIVE",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.9.2/Live/AdyenPOS.xcframework.zip",
-            checksum: "44ecada2812ee3f5aba8da88d3c16787425b930aa17b3b683755fa6956811619"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.10.0/Live/AdyenPOS.xcframework.zip",
+            checksum: "749974d2bc0d4fcdec57bfe4cc3b29c1119a73cc3bd5a90fde23485e48d6470c"
         ),
         .binaryTarget(
             name: "ADYPOSLIVE",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.9.2/Live/ADYPOS.xcframework.zip",
-            checksum: "642946441a8f51f2400d0e5c223ee2d0be2fe51245412de247744e6cb8478425"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.10.0/Live/ADYPOS.xcframework.zip",
+            checksum: "aeaec74a25c607c88e23cc286231e9cc0447511fc6011854f7904ff27477d828"
         )
     ]
 )
