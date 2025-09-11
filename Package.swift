ef1cc17e074c8264d497db1d9f9e27ddfc352344
James <17139737+mcguire-james@users.noreply.github.com>
2025-09-11 17:21:04 +0200

    Release 3.10.1

--- Package.swift
+++ Package.swift
@@ -15,23 +15,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdyenPOSTEST",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.10.0/Test/AdyenPOS.xcframework.zip",
-            checksum: "d06ac6e29d59d27a435010313b609ef26bd16d429bfbedb9d12bf4afa20989fc"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.10.1/Test/AdyenPOS.xcframework.zip",
+            checksum: "7e8d629ddeb79967f604e9abc247e9b3521b445cc87633a808b6636e0efcb701"
         ),
         .binaryTarget(
             name: "ADYPOSTEST",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.10.0/Test/ADYPOS.xcframework.zip",
-            checksum: "b5554750ec88f193701d4c143b2f339a9668eb14956c401ab48e7c100efc12d8"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.10.1/Test/ADYPOS.xcframework.zip",
+            checksum: "bd366baf368f923c143c1335523a2ecba7a796b73cb717c67da75a3eef49348f"
         ),
         .binaryTarget(
             name: "AdyenPOSLIVE",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.10.0/Live/AdyenPOS.xcframework.zip",
-            checksum: "749974d2bc0d4fcdec57bfe4cc3b29c1119a73cc3bd5a90fde23485e48d6470c"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.10.1/Live/AdyenPOS.xcframework.zip",
+            checksum: "d0c55ca085ae788e95f8a4bcac932d6c080c217314bda8038f2e1f8079834d27"
         ),
         .binaryTarget(
             name: "ADYPOSLIVE",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.10.0/Live/ADYPOS.xcframework.zip",
-            checksum: "aeaec74a25c607c88e23cc286231e9cc0447511fc6011854f7904ff27477d828"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.10.1/Live/ADYPOS.xcframework.zip",
+            checksum: "2eeb25f3096a90dfc8aa8ffc62b446b31f79071f9ba6f90970ee246bc4e71e3e"
         )
     ]
 )
