56c1df0aa50f0f95f5a032426096ebfe09b14670
James <17139737+mcguire-james@users.noreply.github.com>
2026-03-18 14:08:33 +0100

    Release 3.15.0

--- Package.swift
+++ Package.swift
@@ -15,23 +15,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdyenPOSTEST",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.14.0/Test/AdyenPOS.xcframework.zip",
-            checksum: "e2736279941fd9d70623a09fdc4a9a3f09d88bd9ef4ded02afc305e5acddd58f"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.15.0/Test/AdyenPOS.xcframework.zip",
+            checksum: "87af3a84bf3c242528d5c42b6b042cd7e6b40395f04946df128e3380adacb381"
         ),
         .binaryTarget(
             name: "ADYPOSTEST",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.14.0/Test/ADYPOS.xcframework.zip",
-            checksum: "868ce9e3f8e745878d8f9c2ec6cc1481b90d226ef162b32d395dea22acb6e0bb"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.15.0/Test/ADYPOS.xcframework.zip",
+            checksum: "2ad184ca0f04c4488a43a8cee9bacc4c95a12fa8beb9632fa6f60074bab23d6f"
         ),
         .binaryTarget(
             name: "AdyenPOSLIVE",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.14.0/Live/AdyenPOS.xcframework.zip",
-            checksum: "ad5f988cbc838ba4d7300ceef95a3feb0d301c669281381b6e959b88599a1b11"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.15.0/Live/AdyenPOS.xcframework.zip",
+            checksum: "efc0fadf21eb8140c845807d46ee859de18c12940141d635ce0d664c09e3c805"
         ),
         .binaryTarget(
             name: "ADYPOSLIVE",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.14.0/Live/ADYPOS.xcframework.zip",
-            checksum: "6dbac3e2b6be5f78ceef0c2ec6bb4694ee4d60af9a66adc9a08011cb1ec4d420"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.15.0/Live/ADYPOS.xcframework.zip",
+            checksum: "62742c4ec70586a6f4e47d95155142bd8cb811f294911cfb7307afc201a5e587"
         )
     ]
 )
