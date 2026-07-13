8fb42ddaf560f0adad2d49a1dad922eb97f0ba88
James <17139737+mcguire-james@users.noreply.github.com>
2026-07-13 16:58:56 +0200

    Release 3.18.0

--- Package.swift
+++ Package.swift
@@ -15,23 +15,23 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AdyenPOSTEST",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.17.0/Test/AdyenPOS.xcframework.zip",
-            checksum: "85a65eff56fd87b266870b90d733fb78c24150083c6a7abe13d566292be23b5d"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.18.0/Test/AdyenPOS.xcframework.zip",
+            checksum: "49dbca710bb417c97af1e50eda56d8f5f842c4216aeaaefde42e4a641e0c4444"
         ),
         .binaryTarget(
             name: "ADYPOSTEST",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.17.0/Test/ADYPOS.xcframework.zip",
-            checksum: "853c639b45f567acc4b9f5d7b4d4158c19b69af6ff532bb04c6d2bdff93ff654"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.18.0/Test/ADYPOS.xcframework.zip",
+            checksum: "2c301c8d15bfc974f44a352b373fe73c02e3f52cd307a51b6581c051005c4538"
         ),
         .binaryTarget(
             name: "AdyenPOSLIVE",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.17.0/Live/AdyenPOS.xcframework.zip",
-            checksum: "566aeb1e832412694e1cd08c950955104b5d910e0e793630ec8d9b74e24af014"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.18.0/Live/AdyenPOS.xcframework.zip",
+            checksum: "ca99dfdd3be4c5259db6f94826bad551752feffff7d7ddb7e6b90a50b41004ff"
         ),
         .binaryTarget(
             name: "ADYPOSLIVE",
-            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.17.0/Live/ADYPOS.xcframework.zip",
-            checksum: "935e17ee508c9192a2b26471f23b02436b206763e966bfff6d140dba58634d23"
+            url: "https://pos-mobile.cdn.adyen.com/adyen-pos-ios/3.18.0/Live/ADYPOS.xcframework.zip",
+            checksum: "ddbb779901a258f428b80c1712dadaf93ba6fabba0ed75b6b82c9523ad500593"
         )
     ]
 )
